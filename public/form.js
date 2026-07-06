4669ff7ac42f21b054f95bd1a953073291e78edf
Gabriela Froń <gabriela.fron04@gmail.com>
2026-07-06 11:38:24 +0200

    Move form script to public JavaScript

---
+++ public/form.js
@@ -0,0 +1,6 @@
+const exerciseForm = document.getElementById('exercise-form')
+
+exerciseForm.addEventListener('submit', () => {
+  const userId = document.getElementById('uid').value
+  exerciseForm.action = `/api/users/${userId}/exercises`
+})
